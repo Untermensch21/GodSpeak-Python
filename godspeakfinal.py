d7d3dd5115ba528d180e8ac24139ffc0b5dd01d5
Untermensch21 <66437924+Untermensch21@users.noreply.github.com>
2020-07-09 22:03:11 +0300

    Update godspeakfinal.py

--- godspeakfinal.py
+++ godspeakfinal.py
@@ -3,24 +3,16 @@ from random import randrange
 import linecache as lc
 
 engine = pyttsx3.init()
-# f=open("godVocab.txt", "r")
+
 
 engine.say("God says \n")
 print("God says")
 
 for i in range(20):
-   # mainstring = " "
     rnum = randrange(710)
     print(lc.getline("godVocab.txt", rnum), end="")
     engine.say(lc.getline("godVocab.txt", rnum))
-   # thestring = f.readline(rnum)
-   # mainstring += (thestring + " " )
-    
-# print("God says:", mainstring)
-    
-#engine = pyttsx3.init()
-#engine.say("God says")
-#engine.say(mainstring)
+
 engine.runAndWait() 
 
 
